447553dfff7f297816ff38e793761fab8b016fbf
Adarsh Pathak <adarsh24699@gmail.com>
2021-09-21 11:05:49 +0530

    Defining Spring bean with java code i.e. via @Bean

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,23 @@
 package com.luv2code.springdemo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
-public class SportConfig {
-	
+//@ComponentScan("com.luv2code.springdemo")
 
+public class SportConfig {
+	// Define a bean for sad fortune service
+	@Bean 
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();	
+	}
+	
+	
+	// Define a bean for our swim coach AND inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+	
 }
